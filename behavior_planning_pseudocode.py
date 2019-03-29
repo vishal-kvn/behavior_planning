eb3c9883f4142dd1b71f7f043b1222e6187ad700
vishal kajjam <vishal.kvn@gmail.com>
2019-03-29 06:07:56 -0400

    First Commit

---
+++ behavior_planning_pseudocode.py
@@ -0,0 +1,56 @@
+def transition_function(predictions, current_fsm_state, current_pose, cost_functions, weights):
+    # only consider states which can be reached from current FSM state.
+    possible_successor_states = successor_states(current_fsm_state)
+
+    # keep track of the total cost of each state.
+    costs = []
+    for state in possible_successor_states:
+        # generate a rough idea of what trajectory we would
+        # follow IF we chose this state.
+        trajectory_for_state = generate_trajectory(state, current_pose, predictions)
+
+        # calculate the "cost" associated with that trajectory.
+        cost_for_state = 0
+        for i in range(len(cost_functions)) :
+            # apply each cost function to the generated trajectory
+            cost_function = cost_functions[i]
+            cost_for_cost_function = cost_function(trajectory_for_state, predictions)
+
+            # multiply the cost by the associated weight
+            weight = weights[i]
+            cost_for_state += weight * cost_for_cost_function
+         costs.append({'state' : state, 'cost' : cost_for_state})
+
+    # Find the minimum cost state.
+    best_next_state = None
+    min_cost = 9999999
+    for i in range(len(possible_successor_states)):
+        state = possible_successor_states[i]
+        cost  = costs[i]
+        if cost < min_cost:
+            min_cost = cost
+            best_next_state = state 
+
+    return best_next_state
+
+double goal_distance_cost(int goal_lane, int intended_lane, int final_lane, 
+                          double distance_to_goal) {
+  // The cost increases with both the distance of intended lane from the goal
+  //   and the distance of the final lane from the goal. The cost of being out 
+  //   of the goal lane also becomes larger as the vehicle approaches the goal.
+  int delta_d = 2.0 * goal_lane - intended_lane - final_lane;
+  double cost = 1 - exp(-(std::abs(delta_d) / distance_to_goal));
+
+  return cost;
+}
+
+double inefficiency_cost(int target_speed, int intended_lane, int final_lane, 
+                         const std::vector<int> &lane_speeds) {
+  // Cost becomes higher for trajectories with intended lane and final lane 
+  //   that have traffic slower than target_speed.
+  double speed_intended = lane_speeds[intended_lane];
+  double speed_final = lane_speeds[final_lane];
+  double cost = (2.0*target_speed - speed_intended - speed_final)/target_speed;
+
+  return cost;
+}
